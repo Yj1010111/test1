4777e26e0f333d32b202df1f97dcd938d8883eb0
Yj1010111 <148938810+Yj1010111@users.noreply.github.com>
2023-11-29 14:14:57 +0900

    Update test.c

--- test.c
+++ test.c
@@ -6,24 +6,21 @@ float add(float x,float y){
 float min(float x,float y){
     return x-y;
 }
-float fmodf(float x,float y){
-    return fmod(x,y);
-}
+
 main(){
     int c;
     float a,b;
     printf("숫자를 입력하시오.");
     scanf("%f %f", &a, &b);
     
-    printf("1.더하기 2. 빼기 3. 나머지 구하기 6.x의 y제곱 ");
+    printf("1.더하기 2. 빼기 3.x의 y제곱 ");
     scanf("%d", &c);
     
     if (c == 1)
-     printf("%f", add(a,b));
+     printf("%0.f", add(a,b));
     else if(c == 2)
-     printf("%f", min(a,b));
+     printf("%0.f", min(a,b));
     else if(c == 3)
-     printf("%f", fmodf(a,b));
-    else if(c == 6)
-     printf("%f", pow(a,b));
+     printf("%0.f", pow(a,b));
+
 }
