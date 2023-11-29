94f9adf88028e9a3384adc67d199df73d730ea94
Yj1010111 <148938810+Yj1010111@users.noreply.github.com>
2023-11-29 14:09:16 +0900

    Update test.c

--- test.c
+++ test.c
@@ -15,7 +15,7 @@ main(){
     printf("숫자를 입력하시오.");
     scanf("%f %f", &a, &b);
     
-    printf("1.더하기 2. 빼기 3. 나머지 구하기");
+    printf("1.더하기 2. 빼기 3. 나머지 구하기 6.x의 y제곱 ");
     scanf("%d", &c);
     
     if (c == 1)
@@ -24,4 +24,6 @@ main(){
      printf("%f", min(a,b));
     else if(c == 3)
      printf("%f", fmodf(a,b));
+    else if(c == 6)
+     printf("%f", pow(a,b));
 }
